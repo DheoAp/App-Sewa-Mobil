ceb513624bacbd555e963d089d2c8bca2e923098
Dheo Apriansyah <dheoap21@gmail.com>
2020-12-08 09:16:04 +0700

    tambah akses_login_cs()

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+  public function __construct()
+  {
+    parent::__construct();
+    akses_login_cs();
+  }
+
 	public function index()
   {
     $data['dataMobil'] = $this->rental_model->get_data('mobil')->result();
